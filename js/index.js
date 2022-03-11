8f9d11e2788c63a27e13f485bea11202e3e7c6ca
Justin <jstn.anthony1@gmail.com>
2022-03-11 14:39:41 -0500

    added delete book feature

--- js/index.js
+++ js/index.js
@@ -3,16 +3,24 @@ let titleDisplay = document.querySelector("#new-book-title");
 let authorDisplay = document.querySelector("#new-book-author");
 let pagesDisplay = document.querySelector("#new-book-pages");
 let readDisplay = document.querySelector("#new-book-read");
-let myLibrary = [{title: 'yo', author: 'test', pages: 20, read: 'reading'}, 
-                {title: 'the Midnight Library', author: 'Matt Haig', pages: 308, read: 'finished'}];
-// let myLibrary = [];
+
+// let myLibrary = [
+//   { title: "yo", author: "test", pages: 20, read: "reading" },
+//   {
+//     title: "the Midnight Library",
+//     author: "Matt Haig",
+//     pages: 308,
+//     read: "finished",
+//   },
+// ];
+let myLibrary = [];
 button.addEventListener("click", addBookToLibrary);
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.pages = pages;
+  this.read = read;
   //   this.info = function () {
   //     return `${title}`;
   //   };
@@ -29,30 +37,48 @@ function addBookToLibrary() {
   // console.log(test.title);
   myLibrary.push(test);
   // displayBooks();
-  displayNewBook(test)
-
+  displayNewBook(test);
 }
 
-function displayNewBook(newBook){
+function displayNewBook(newBook) {
   let bookList = document.querySelector("tbody");
-    bookList.innerHTML += `<tr> <td>${newBook.title} </td> <td> ${newBook.author} </td>
-        <td> ${newBook.pages} </td> <td> ${newBook.read} </td>`
+  // bookList.innerHTML += `<tr id="newBook"> <td>${newBook.title} </td> <td> ${newBook.author} </td>
+  //       <td> ${newBook.pages} </td> <td> ${newBook.read} </td> </tr>`;
+  let newRow = document.createElement("tr");
+  let titleData = document.createElement("td");
+  let authorData = document.createElement("td");
+  let pageData = document.createElement("td");
+  let progressData = document.createElement("td");
+  let title = document.createTextNode(newBook.title);
+  let author = document.createTextNode(newBook.author);
+  let pages = document.createTextNode(newBook.pages);
+  let progress = document.createTextNode(newBook.read);
+
+  bookList.appendChild(newRow);
+  newRow.classList.add("newBook");
+  newRow.appendChild(titleData);
+  newRow.appendChild(authorData);
+  newRow.appendChild(pageData);
+  newRow.appendChild(progressData);
+  titleData.appendChild(title);
+  authorData.appendChild(author);
+  pageData.appendChild(pages);
+  progressData.appendChild(progress);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  newRow.appendChild(deleteBtn);
+  deleteBtn.addEventListener("click", () => {
+    newRow.remove();
+  });
 }
 
-function displayBooks(){
-  let bookList = document.querySelector("tbody");
-  for (let i = 0; i <= myLibrary.length; i++){
-    bookList.innerHTML += `<tr> <td>${myLibrary[i].title} </td> <td> ${myLibrary[i].author} </td>
-   <td>${myLibrary[i].pages} </td> <td>${myLibrary[i].read} </td> </tr>
-    ` ;
-  }
-
-}
-
-displayBooks();
-
-
-
-
-
-
+// function displayBooks() {
+//   let bookList = document.querySelector("tbody");
+//   for (let i = 0; i < myLibrary.length; i++) {
+//     bookList.innerHTML += `<tr class="newBook"> <td>${myLibrary[i].title} </td> <td> ${myLibrary[i].author} </td>
+//    <td>${myLibrary[i].pages} </td> <td>${myLibrary[i].read} </td> </tr>
+//     `;
+//   }
+// }
+// displayBooks();
